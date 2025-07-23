35f797b070f2d999806abb93300cae9c72babe06
kabinh07 <kanchon@polygontech.xyz>
2025-07-23 16:17:40 +0000

    ADDED other output types

--- tesseract/handler.py
+++ tesseract/handler.py
@@ -5,6 +5,7 @@ import torch
 import base64
 
 from ts.torch_handler.base_handler import BaseHandler
+from ts.utils.util import PredictionException
 
 class TesseractOCRHandler(BaseHandler):
     def __init__(self):
@@ -14,13 +15,17 @@ class TesseractOCRHandler(BaseHandler):
 
     def initialize(self, context):
         self.manifest = context.manifest
-        properties = context.system_properties
-        model_dir = properties.get("model_dir")
         self.initialized = True
 
     def preprocess(self, data):
         data = data[0].get("data") or data[0].get("body")
         base_64_images = data.get("images")
+        if not base_64_images:
+            raise PredictionException("No images found in request")
+        if not isinstance(base_64_images, list):
+            base_64_images = [base_64_images]
+        self.language = data.get("language", "ben+eng")
+        self.output_type = data.get("output_type", "txt")
         images = []
         for image_data in base_64_images:
             if len(image_data.split(',')) > 1:
@@ -34,9 +39,32 @@ class TesseractOCRHandler(BaseHandler):
     def inference(self, data):
         results = []
         for image in data:
-            text = pytesseract.image_to_string(image, lang="ben+eng")
-            results.append(text)
-        return  [{"results": results}]
+            try:
+                if isinstance(self.output_type, str) and self.output_type == "txt":
+                    output = pytesseract.image_to_string(image, lang=self.language)
+                    results.append({"txt": output})
+                elif isinstance(self.output_type, str) and self.output_type == "box":
+                    output = pytesseract.image_to_boxes(image, lang=self.language)
+                    results.append({"box": output})
+                elif isinstance(self.output_type, str) and self.output_type == "data":
+                    output = pytesseract.image_to_data(image, lang=self.language)
+                    results.append({"data": output})
+                elif isinstance(self.output_type, str) and self.output_type == "osd":
+                    output = pytesseract.image_to_osd(image, lang=self.language)
+                    results.append({"osd": output})
+                elif isinstance(self.output_type, list):
+                    output = pytesseract.run_and_get_multiple_output(image, lang=self.language, extensions=self.output_type)
+                    out_dict = {}
+                    for idx, item in enumerate(self.output_type):
+                        out_dict[item] = output[idx]
+                    results.append(out_dict)
+                else:
+                    results.append({"error": "Invalid output type"})
+            except Exception as e:
+                results.append({"error": str(e)})
+        return self.postprocess(results)
 
     def postprocess(self, data):
-        return data
+        if isinstance(data, list):
+            return data
+        return [data]
